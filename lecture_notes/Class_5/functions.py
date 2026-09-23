0398d84906330f55b0157cd6bd9c4a3f3179de53
Nolan <nolanfreyer@berkeley.edu>
2026-09-23 15:04:07 -0700

    lecture 5

---
+++ lecture_notes/Class_5/functions.py
@@ -0,0 +1,77 @@
+
+# name = "Nolan"
+
+# def say_hello(name):
+#     print("Hello,", name)
+
+# say_hello(name)
+
+# def add(a,b):
+#     return a+b
+
+# added_number = add(7,8)
+
+# print(add(7,8))
+
+# print(added_number)
+
+# x=8
+
+# if x%2==0:
+#     print("x is even")
+# else:
+#     print("x is odd")
+
+# def check_num(num):
+#     if num>0:
+#         return "Positive"
+#     elif num<0:
+#         return "Negative"
+#     else:
+#         return "Zero"
+    
+# print(check_num(42))
+
+# def can_vote(age, is_citizen):
+#     if age >= 18 and is_citizen:
+#         print("You can vote")
+#     else:
+#         print("You cannot vote")
+
+# can_vote(18, True)
+
+# def is_weekend(day):
+#     if day=="Saturday" or day=="Sunday":
+#         return "It is the weekend!"
+#     elif day=="Friday":
+#         return "ehhhhh depends on who you ask"
+#     else:
+#         return "It is not the weekend :("
+
+# print(is_weekend("Friday"))
+
+# for i in range(10):
+#     print(i)
+
+# fruit_basket = ["lychee", "mango", "nectarines"]
+
+# for fruit in fruit_basket:
+#     print(fruit)
+
+# def countdown(start):
+#     while start > 0:
+#         print("T -", start)
+#         start -= 1
+#     print("Lift off!")
+
+# countdown(10)
+
+# def weather(temp):
+#     if 65<temp<80:
+#         print("Perfect weather today!")
+#     elif 80<=temp:
+#         print("It's hot today!")
+#     else:
+#         print("It's cold today!")
+
+# weather(80)
